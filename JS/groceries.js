ae725c42ac36463e1d8204f3967c876c3b71d6fc
Thierry Laprade <tlapr093@uottawa.ca>
2020-05-23 20:49:56 -0400

    initial commit

---
+++ JS/groceries.js
@@ -0,0 +1,26 @@
+/* Une portion de ce code a été pris de https://github.com/carolinebarriere/carolinebarriere.github.io/tree/master/SEG3125-Module2-Grocery (Caroline Barriere) */
+
+// Array of products, each product is an object with different fieldset
+// A set of ingredients should be added to products		 
+
+var products = [
+	{
+		name: "brocoli",
+		vegetarian: true,
+		glutenFree: true,
+		price: 1.99
+	},
+	{
+		name: "bread",
+		vegetarian: true,
+		glutenFree: false,
+		price: 2.35
+	},
+	{
+		name: "salmon",
+		vegetarian: false,
+		glutenFree: true,
+		price: 10.00
+	}
+];
+	
